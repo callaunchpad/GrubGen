562b9e92d9fbefa0f5b54f5e6e5b90c0495641af
chiangvincent <vincentchiang@berkeley.edu>
2019-04-18 23:29:57 -0700

    fixed bug with rgb func, tuples not loading correctly

--- src/python/preprocessor/preprocessor.py
+++ src/python/preprocessor/preprocessor.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
 from PIL import Image
 import glob
 import os
 import numpy as np
 import os
 from PIL import Image
+from resizeimage import resizeimage
+import cv2
 
-resources = "../../../resources"
 
+# In[12]:
 
-def is_img_name(name):
-    if name.endswith("png"):
-        print("unexpected png, please add feature")
-        1/0
-    return name.endswith("jpg")
 
 '''
 params 
@@ -23,15 +25,18 @@ return
         keys are "baklava" 
         values are "./UPMC_Food101/images/train/baklava/baklava_808.jpg"
 '''
-def create_food_dictionary(main_directory, food_types):
-    for subdir in os.listdir(main_directory):
-        food_name = subdir
+def create_food_dictionary(main_directory, food_types): 
+    for root, dirs, files in os.walk(main_directory, topdown=False):
+        food_name = root.split('/')[-1] # get the last string of something like "./UPMC_Food101/images/train/baklava"
         if food_name not in food_types:
             food_types[food_name] = []
-        for food_img_path in os.listdir(os.path.join(main_directory, food_name)):
-            if is_img_name(food_img_path):
-                food_types[food_name].append(os.path.join(main_directory, food_name, food_img_path))
+        for root2, dirs, files in os.walk(root, topdown=False):
+            for name in files:
+                food_types[food_name].append(os.path.join(root2, name))
     return food_types
+
+
+# In[13]:
 
 
 '''
@@ -40,25 +45,42 @@ params
 returns 
     all the images in that list stacked. Each row is a flattened image that contains (R,G,B)
 '''
-def create_stacked_images(img_list, size): 
+def create_stacked_images(img_list): 
     final_array = [] 
     for image_path in img_list:
-        im = Image.open(image_path)
+        im=Image.open(image_path)
         cropped_image = crop(im)
-        resized = resize(cropped_image, size)
-        final_array.append(np.array(resized)) 
-    return np.stack(final_array)      
+        resized = resize(cropped_image, 64)
+        final_array.append(rgb(resized, 64, 64)) 
+    return final_array
+
+
+# In[14]:
+
 
 # returns an array of rgb tuples for one image 
 # returns the flattened image
+# returned image is 64, 64, 3
 def rgb(image, width, length):
-    1/0 #don't use this
-    flattened = []
-    for i in range(width): 
-        for j in range(length):
-            new = image.load()[i, j]
-            flattened.append(new)
-    return np.array(flattened)
+    rgb_im = np.array(image).ravel()
+    reshaped = rgb_im.reshape(64, 64, 3)
+    return np.array(reshaped)
+
+
+# In[15]:
+
+
+# def crop(im, new_width, new_height):
+#     width, height = im.size
+#     left = (width - new_width)/2
+#     top = (height - new_height)/2
+#     right = (width + new_width)/2
+#     bottom = (height + new_height)/2
+#     return im.crop((left, top, right, bottom))
+
+
+# In[16]:
+
 
 # takes in an PIL Image object 
 # returns an Image object that is cropped 
@@ -66,61 +88,130 @@ def rgb(image, width, length):
 def crop(im):
     width, height = im.size
     if width > height: 
-        left = (width - height)//2
+        left = (width - height)/2
         top = 0
-        right = (width + height)//2
+        right = (width + height)/2
         bottom = height
     else: 
         left = 0
-        top = (height - width)//2
+        top = (height - width)/2
         right = width
-        bottom = (height + width)//2
-    im = im.crop((left, top, right, bottom))
-    assert im.size[0] == im.size[1], im.size
-    return im
+        bottom = (height + width)/2
+    return im.crop((left, top, right, bottom))
 
 
-def resize(im, size):
-    im = im.resize((size, size), Image.ANTIALIAS)
-    return im
+# In[17]:
+
+
+def resize(im, size, padColor=0):
+    desired_size = size
+
+    old_size = im.size  # old_size[0] is in (width, height) format
+
+    ratio = float(desired_size)/max(old_size)
+    new_size = tuple([int(x*ratio) for x in old_size])
+    # use thumbnail() or resize() method to resize the input image
+
+    # thumbnail is a in-place operation
+
+    # im.thumbnail(new_size, Image.ANTIALIAS)
+
+    im = im.resize(new_size, Image.ANTIALIAS)
+    # create a new image and paste the resized on it
+
+    new_im = Image.new("RGB", (desired_size, desired_size))
+    new_im.paste(im, ((desired_size-new_size[0])//2,
+                        (desired_size-new_size[1])//2))
+
+    return new_im
+
+
+# In[18]:
+
+
+# im=Image.open('../../../../testpic.jpg')
+# im = crop(im)
+# resize(im, 64)
+
+
+# In[19]:
 
 
 # take in an array and output it to a numpy file 
-def create_np_file(array, name, overwrite=False):
-    if overwrite or not os.path.isfile(name): 
-        print(name, "written")
-        np.save(name, array)
-    else:
-        print("Already exists", name, "skipping...")
+def create_np_file(array, name):
+    np.save(name, array)
+    return
+
+
+# In[20]:
+
 
 # creates .npy files for each array in dictionary, saves them in the folder specified by the path to folder param 
 # saving in folder "food_lists" = './food_lists'
 # takes care of cropping of all the images specified in the dictionary 
-def create_files(food_dict, path_to_folder, size, overwrite): 
-    for key, food_dir_list in food_dict.items():
-        if len(food_dir_list) < 1:
-            continue
-        array = create_stacked_images(food_dir_list, size)
-        create_np_file(array, path_to_folder + '/' + key + '.npy', overwrite)
+def create_files(food_dict, path_to_folder): 
+    for key, value in food_dict.items():
+        array = create_stacked_images(food_dict[key])
+        if not os.path.isfile(path_to_folder + '/' + key + '.npy'): 
+            create_np_file(array, path_to_folder + '/' + key + '.npy')
+    return 
 
-if __name__ == '__main__':
-    import sys
-    assert len(sys.argv) < 4
-    size = 64
-    overwrite = False
-    if len(sys.argv) > 1:
-        size = int(sys.argv[1])
-    if len(sys.argv) == 3:
-        assert sys.argv[2] =='0' or sys.argv[2] =='1'
-        overwrite = bool(sys.argv[2])
-        if overwrite:
-            print("Overwrite flag on")
 
-    final_food_dict = {} 
-    final_food_dict = create_food_dictionary(resources + "/images/train", final_food_dict) 
-    final_food_dict = create_food_dictionary(resources + "/images/test", final_food_dict)
+# # code to run the images
 
-    create_files(final_food_dict, resources + "/processed", size, overwrite)
+# In[21]:
+
+
+final_food_dict = {} 
+final_food_dict = create_food_dictionary("../../../resources/images/train", final_food_dict) 
+final_food_dict = create_food_dictionary("../../../resources/images/test", final_food_dict)
+
+
+# In[22]:
+
+
+create_files(final_food_dict, '../../../resources/processed')
+
+
+# In[25]:
+
+
+test = create_stacked_images(final_food_dict["baklava"])
+
+
+# In[68]:
+
+
+# create_np_file(test, "./np_array_outputs/baklava.npy")
+
+
+# # Old Code
+
+# In[101]:
+
+
+# # crop at the center 64, both train and test 
+# # need to find a way to import all images in a directory 
+
+# image_list = []
+# cropped_rgb_values = []
+# # images stores all the paths of images in the apple_pie folder 
+# for path in images: 
+#     for filename in glob.glob(path): # for all the images in the apple_pie folder 
+#         im=Image.open(filename)
+#         image_list.append(im)
+#         pix = im.load()
+
+#         #crop it after
+#         # Get the RGBA Value of the a pixel of an image
+# #         pix[x,y] = value  # Set the RGBA Value of the image (tuple)
+#     #     im.save('alive_parrot.png') 
+#         image = crop(im, 64, 64)
+#         array = rgb(image, 64, 64)
+
+
+# In[ ]:
+
 
 
 
