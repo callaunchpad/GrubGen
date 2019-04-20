0578cc77cc112c260d488068fa78ab8694496ca2
Pardhu Konakanchi <pardhukonakanchi@ocf.berkeley.edu>
2019-04-20 13:24:34 -0700

    More npy testing

--- src/python/model/DCGAN/DCGANfinal.py
+++ src/python/model/DCGAN/DCGANfinal.py
@@ -16,7 +16,7 @@ print('Hello World')
 
 channels = 3
 
-def generator(z, reuse=None):
+def generator(z,training, reuse=None):
 	with tf.variable_scope('gen',reuse=reuse):
 		""" This is the generator model that is sepcifically designed to ouput 64x64 size images with the desired channels. """
 
@@ -25,27 +25,27 @@ def generator(z, reuse=None):
 		momentum = 0.99
 		#is_training=True
 		hidden1=tf.layers.conv2d_transpose(inputs=z, kernel_size=[4,4], filters=1028*2, strides=(1, 1), padding='valid', activation=tf.nn.leaky_relu)
-		batch_norm1 = tf.contrib.layers.batch_norm(hidden1, decay=momentum)
-		hidden2=tf.layers.conv2d_transpose(inputs=batch_norm1, kernel_size=[4,4], filters=512*2, strides=(2, 2), padding='same', activation=tf.nn.leaky_relu)
-		batch_norm2 = tf.contrib.layers.batch_norm(hidden2, decay=momentum)
-		hidden3=tf.layers.conv2d_transpose(inputs=batch_norm2, kernel_size=[4,4], filters=256*2, strides=(2, 2), padding='same', activation=tf.nn.leaky_relu)
-		batch_norm3 = tf.contrib.layers.batch_norm(hidden3, decay=momentum)
-		hidden4=tf.layers.conv2d_transpose(inputs=batch_norm3, kernel_size=[4,4], filters=128*2, strides=(2, 2), padding='same', activation=tf.nn.leaky_relu)
-		batch_norm4 = tf.contrib.layers.batch_norm(hidden4, decay=momentum)
-		output=tf.layers.conv2d_transpose(inputs=batch_norm4, kernel_size=[4,4], filters=channels, strides=(2, 2), padding='same', activation=tf.nn.tanh)
+		batch_norm1 = tf.contrib.layers.batch_norm(hidden1, is_training=training, decay=momentum)
+		hidden2=tf.layers.conv2d_transpose(inputs=batch_norm1, kernel_size=5, filters=512*2, strides=(2, 2), padding='same', activation=tf.nn.leaky_relu)
+		batch_norm2 = tf.contrib.layers.batch_norm(hidden2, is_training=training, decay=momentum)
+		hidden3=tf.layers.conv2d_transpose(inputs=batch_norm2, kernel_size=5, filters=256*2, strides=(2, 2), padding='same', activation=tf.nn.leaky_relu)
+		batch_norm3 = tf.contrib.layers.batch_norm(hidden3, is_training=training, decay=momentum)
+		hidden4=tf.layers.conv2d_transpose(inputs=batch_norm3, kernel_size=5, filters=128*2, strides=(2, 2), padding='same', activation=tf.nn.leaky_relu)
+		batch_norm4 = tf.contrib.layers.batch_norm(hidden4, is_training=training, decay=momentum)
+		output=tf.layers.conv2d_transpose(inputs=batch_norm4, kernel_size=5, filters=channels, strides=(2, 2), padding='same', activation=tf.nn.tanh)
 		return output
 def discriminator(X, reuse=None):
     with tf.variable_scope('dis',reuse=reuse):
     	momentum = 0.99
     	#X = tf.reshape(X, shape=[-1, 64, 64, channels])
-    	hidden1=tf.layers.conv2d(inputs=X, kernel_size=4, filters=128*2, strides=2, padding='same', activation=tf.nn.leaky_relu)
+    	hidden1=tf.layers.conv2d(inputs=X, kernel_size=5, filters=128, strides=2, padding='same', activation=tf.nn.leaky_relu)
     	batch_norm1 = tf.contrib.layers.batch_norm(hidden1, decay=momentum)
-    	hidden2=tf.layers.conv2d(inputs=batch_norm1, kernel_size=4, filters=256*2,strides=2, padding='same', activation=tf.nn.leaky_relu)
+    	hidden2=tf.layers.conv2d(inputs=batch_norm1, kernel_size=5, filters=256,strides=2, padding='same', activation=tf.nn.leaky_relu)
     	batch_norm2 = tf.contrib.layers.batch_norm(hidden2, decay=momentum)
-    	hidden3=tf.layers.conv2d(inputs=batch_norm2, kernel_size=4, filters=512*2,strides=2, padding='same', activation=tf.nn.leaky_relu)
+    	hidden3=tf.layers.conv2d(inputs=batch_norm2, kernel_size=5, filters=512,strides=2, padding='same', activation=tf.nn.leaky_relu)
     	batch_norm3 = tf.contrib.layers.batch_norm(hidden3, decay=momentum)
     	#x_flat = tf.contrib.layers.flatten(batch_norm3)  	
-    	logits=tf.layers.conv2d(inputs=batch_norm3, kernel_size=4, filters=1, strides=1, padding='valid')
+    	logits=tf.layers.conv2d(inputs=batch_norm3, kernel_size=5, filters=1, strides=1, padding='valid')
     	output=tf.sigmoid(logits)
     	return output, logits
 
@@ -53,36 +53,38 @@ tf.reset_default_graph()
 
 real_images=tf.placeholder(tf.float32,shape=[None, 64, 64, channels])
 z=tf.placeholder(tf.float32,shape=[None, 1, 1, 100])
+training=tf.placeholder(tf.bool)
 
-#noisy_input_real = real_images + tf.random_normal(shape=tf.shape(real_images), mean=0.0, stddev=random.uniform(0.0, 0.1), dtype=tf.float32)
 
-G=generator(z)
-D_output_real,D_logits_real=discriminator(real_images)
+noisy_input_real = real_images + tf.random_normal(shape=tf.shape(real_images), mean=0.0, stddev=random.uniform(0.0, 0.2), dtype=tf.float32)
+
+G=generator(z, training)
+D_output_real,D_logits_real=discriminator(noisy_input_real)
 D_output_fake,D_logits_fake=discriminator(G,reuse=True)
 
 def loss_func(logits_in, labels_in):
 	return tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits_in,labels=labels_in))
 
-D_real_loss=loss_func(D_logits_real, tf.ones_like(D_logits_real))
+D_real_loss=loss_func(D_logits_real, tf.ones_like(D_logits_real)*random.uniform(0.9, 1.0))
 D_fake_loss=loss_func(D_logits_fake, tf.zeros_like(D_logits_fake))
-D_loss = D_real_loss + D_fake_loss
+D_loss = (D_real_loss + D_fake_loss)
 
 G_loss = loss_func(D_logits_fake, tf.ones_like(D_logits_fake))
 
 lr_g = 0.0004
-lr_d = 0.00004
+lr_d = 0.0002
 
 tvars = tf.trainable_variables()
 d_vars=[var for var in tvars if 'dis' in var.name]
 g_vars=[var for var in tvars if 'gen' in var.name]
 
-D_trainer=tf.train.AdamOptimizer(lr_d).minimize(D_loss,var_list=d_vars)
-G_trainer=tf.train.AdamOptimizer(lr_g).minimize(G_loss,var_list=g_vars)
+D_trainer=tf.train.AdamOptimizer(lr_d, beta1=0.5).minimize(D_loss,var_list=d_vars)
+G_trainer=tf.train.AdamOptimizer(lr_g, beta1=0.5).minimize(G_loss,var_list=g_vars)
 
 
 num_batches=40
 batch_size=100
-epochs=150
+epochs=50
 init=tf.global_variables_initializer()
 
 gen_samples=[]
@@ -98,6 +100,8 @@ train_hist['total_ptime'] = []
 load_files()
 
 rl_images = np.load("../../../../resources/processed/baklava.npy")
+rl_images = (rl_images - 127.5) / 127.5
+
 
 with tf.Session() as sess:
     sess.run(init)
@@ -114,24 +118,25 @@ with tf.Session() as sess:
             train_g=True
             train_d=True
             batch_images = rl_images[i*batch_size:(i+1)*batch_size]
+            #np.save('image_test', batch_images[2])
             #print('just got batch')
             batch_images = np.reshape(batch_images, [-1, 64, 64, 3])
             batch_z=np.random.uniform(-1, 1, size=(batch_size, 1, 1, 100))
-            loss_d_ = sess.run([D_loss], {real_images: batch_images, z: batch_z})
+            loss_d_, _ = sess.run([D_loss, D_trainer], {real_images: batch_images, z: batch_z, training: True})
             #print('just ran loss')
             D_losses.append(loss_d_)
-            z_ = np.random.normal(0, 1, (batch_size, 1, 1, 100))
-            loss_g_ = sess.run([G_loss], {z: batch_z, real_images: batch_images})
+            #z_ = np.random.normal(-1, 1, (batch_size, 1, 1, 100))
+            loss_g_, _ = sess.run([G_loss, G_trainer], {z: batch_z, real_images: batch_images, training: True})
             #print('just ran gen loss')
             G_losses.append(loss_g_)
-            if loss_d_ > loss_g_ * 2:
-                train_g = False
-            if loss_g_ > loss_d_ * 2:
-                train_d = False
-            if train_d:
-                _ = sess.run([D_trainer], {real_images: batch_images, z: batch_z})
-            if train_g:
-                _ = sess.run([G_trainer], {real_images: batch_images, z: batch_z})
+            #if loss_d_ > loss_g_ * 2:
+             #   train_g = False
+            #if loss_g_ > loss_d_ * 2:
+             #   train_d = False
+           # if train_d:
+            #    _ = sess.run([D_trainer], {real_images: batch_images, z: batch_z, training: True})
+           # if train_g:
+           #     _ = sess.run([G_trainer], {real_images: batch_images, z: batch_z, training: True})
             #print('finished training batch')
             epoch_end_time = time.time()
             per_epoch_ptime = epoch_end_time - epoch_start_time
@@ -142,17 +147,17 @@ with tf.Session() as sess:
         train_hist['per_epoch_ptimes'].append(per_epoch_ptime)
             
         sample_z=np.random.uniform(-1,1,size=(1, 1, 1, 100))
-        gen_sample=sess.run(generator(z, reuse=True), feed_dict={z:sample_z})
+        gen_sample=sess.run(generator(z, training, reuse=True), feed_dict={z:sample_z, training: False})
         gen_samples.append(gen_sample)
 
 
 
 reshaped_rgb = gen_samples[0].reshape(64, 64, 3)
-np.save('reshaped_rgb_baklava6', reshaped_rgb)
+np.save('gen_samples_baklava_no_freeze_noisy_ones_and_training', gen_samples)
 img = Image.fromarray(reshaped_rgb, 'RGB')
 img.show()
 reshaped_rgb_last = gen_samples[epochs-1].reshape(64, 64, 3)
-np.save('reshaped_rgb_last_baklava6', reshaped_rgb_last)
+#np.save('reshaped_rgb_last_no_freeze3', reshaped_rgb_last)
 img_last = Image.fromarray(reshaped_rgb_last, 'RGB')
 img_last.show()
 
